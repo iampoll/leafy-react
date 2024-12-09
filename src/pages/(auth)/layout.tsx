5acb2e21566e9aa3661c0a0e34fc2a0b3ea46fc0
ampol <memetitv01@gmail.com>
2024-12-10 05:59:28 +0800

    Refactor AuthLayout for token-based navigation
    - Replaced the Navigate component with useNavigate for handling redirection based on token presence.
    - Implemented useEffect to manage navigation to the dashboard when a token is detected, improving code clarity and responsiveness.
    - Updated the back navigation button to utilize useNavigate for consistent navigation behavior.
    
    These changes enhance the user experience by ensuring authenticated users are redirected appropriately while maintaining a clean and efficient code structure.

--- src/pages/(auth)/layout.tsx
+++ src/pages/(auth)/layout.tsx
@@ -1,19 +1,22 @@
-import { Navigate, Outlet } from "react-router-dom";
-
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-react";
 
 const AuthLayout = () => {
+    const navigate = useNavigate();
     const isHaveToken = localStorage.getItem("token");
 
-    if (isHaveToken) {
-        return <Navigate to="/dashboard" />;
-    }
+    useEffect(() => {
+        if (isHaveToken) {
+            navigate("/dashboard");
+        }
+    }, [isHaveToken, navigate]);
 
     return (
         <section className="h-[100svh] flex flex-col justify-around p-4 lg:px-16">
             <header>
-                <Button variant="ghost" onClick={() => history.back()}>
+                <Button variant="ghost" onClick={() => navigate(-1)}>
                     <ChevronLeft />
                     Back
                 </Button>
